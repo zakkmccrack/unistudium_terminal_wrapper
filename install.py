2d15d52e5155974cac796841cde1b288e9c7e577
zakkmccrack <zaganelli.alessandro.wm@gmail.com>
2025-11-26 23:36:39 +0100

    major changes

---
+++ install.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+
+
+def install(package):
+    ## change yay to a venv type of installation or pip --user mode
+    ## venv can be more portable but needs more code
+    ## pip user mode can create conflict if user has different versions of these packages
+    subprocess.check_call(["yay", "-S", "--noconfirm", package])
+
+
+deps = ["python-bs4", "python-requests", "python-playwright"]
+
+for d in deps:
+    print("Installing {d}...")
+    install(d)
+
+print("Installing playwright browsers...")
+subprocess.run(["playwright", "install"], check=False)
+
+print("Done.")
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def run(playwright: Playwright):
     page = context.new_page()
     ## TODO: Far si che non si crei la pagina iniziale blank
     ## IDEA: -non usare persistent_context?? (dopo potrebbe non tenere i cookie però...)
-    ## 
+    ##
     context.clear_cookies()
     page.goto("https://unistudium.unipg.it/unistudium/local/login/index.php")
     page.wait_for_url("https://unistudium.unipg.it/unistudium/")
@@ -36,7 +36,42 @@ def run(playwright: Playwright):
 def course(cookie: dict, url):
     cookies = dict(MoodleSessionpg_unistudium_prod=cookie)
     r = requests.get(url, cookies=cookies)
-    print(r.text)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    ##print(r.text)
+    sections = []
+
+    for section in soup.select("li.section.course-section"):
+        nome_sezione = section.select_one("h3.sectionname a").get_text(strip=True)
+
+        activity = []
+
+        for item in section.select("li.activity"):
+            link_tag = item.select_one("a")
+            if not link_tag:
+                continue
+
+            titolo = link_tag.get_text(strip=True)
+            url = link_tag["href"]
+
+            activity.append({"titolo": titolo, "url": url})
+
+        sections.append({"sezione": nome_sezione, "attività": activity})
+
+    sectionCount = 0
+    activityCount = 0
+    for sec in sections:
+        print("SEZIONE ", sectionCount, ": ", sec["sezione"])
+        for att in sec["attività"]:
+            print(sectionCount, "-", activityCount, att["titolo"], "→", att["url"])
+            activityCount += 1
+        print("\n")
+        activityCount = 0
+        sectionCount += 1
+
+    values = input("Selezione [SEZIONE]-[ATTIVITA'] -> ").split("-")
+    print(sections[int(values[0])]["attività"][int(values[1])]["url"])
 
 
 with sync_playwright() as playwright:
@@ -45,17 +80,16 @@ with sync_playwright() as playwright:
     cookies = dict(MoodleSessionpg_unistudium_prod=cookie)
     r = requests.get(url, cookies=cookies)
     soup = BeautifulSoup(r.text, "html.parser")
-
     corsi = []
-    
-    corsi=[h3.get_text(strip=True) for h3 in soup.select("h3.coursename a")]
+
+    corsi = [h3.get_text(strip=True) for h3 in soup.select("h3.coursename a")]
 
     links = [a["href"] for a in soup.select("h3.coursename a")]
-    print(links[0])
-    
+
     cnt = 0
     for nome in corsi:
-        print(cnt, "] ", nome, " [link: ", links[cnt], "]")
-        cnt = cnt + 1
+        print(cnt, "] ", nome)
+        cnt += 1
+
     corso = int(input("Numero corso su cui vuoi entrare -> "))
     course(cookie, links[corso])
